ea42b4b696112422994da9420d5f0feaebb38467
David Chase <dchasepdx@gmail.com>
2016-10-23 16:48:21 -0700

    initial commit

---
+++ requestHandlers.js
@@ -0,0 +1,10 @@
+function start() {
+  console.log('Request handler \'start\' was called.');
+}
+
+function upload() {
+  console.log('Request handler \'start\' was called.');
+}
+
+exports.start = start;
+exports.upload = upload;
